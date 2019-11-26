163797b4ed37f8fc34a9d62bc0562828bc3b2981
Alex Wong <alexw14@gmail.com>
2019-11-25 22:17:57 -0800

    add Selection Sort

---
+++ Sorting/SelectionSort.js
@@ -0,0 +1,22 @@
+// Store the first element as the smallest value
+// Compare to next element in the array until a smaller value is found
+// If smaller value is found, designate that to be the new smallest value
+// Swap the smallest value with the first element
+// Repeat with next element until array is sorted
+
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let min = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[min]) {
+        min = j;
+      }
+    }
+    let temp = arr[i];
+    arr[i] = arr[min];
+    arr[min] = temp;
+  }
+  return arr;
+}
+
+console.log(selectionSort([9, 6, 1, 7, 4, 5, 8, 3, 10, 2]));
